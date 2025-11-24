872cd971cc429403f5c07601428d2ac939d8ed43
Michael Bulava <mbulava@gmail.com>
2025-11-23 21:34:48 -0600

    get a new build

--- wrapper/pgquery_wrapper.c
+++
@@ -1,9 +0,0 @@
-#include "pgquery_wrapper.h"
-
-PgQueryParseResult pg_query_parse_wrapper(const char* input) {
-    return pg_query_parse(input);
-}
-
-void pg_query_free_parse_result_wrapper(PgQueryParseResult result) {
-    pg_query_free_parse_result(result);
-}
